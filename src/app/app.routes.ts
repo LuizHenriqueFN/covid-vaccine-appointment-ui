8e1c703ceb85250e635711614c676e55bb3e717c
LuizHenriqueFN <luizhenriqueneves81@gmail.com>
2024-07-17 17:45:21 -0300

    #2 Criação da tela inicial do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AppointmentListComponent } from './pages/appointment-list/appointment-list.component';
+import { PatientListComponent } from './pages/patient-list/patient-list.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'appointment-list', component: AppointmentListComponent },
+  { path: 'patient-list', component: PatientListComponent },
 ];
