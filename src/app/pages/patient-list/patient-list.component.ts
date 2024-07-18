de219d541ed29baed38376a81e66778bd47ddd57
LuizHenriqueFN <luizhenriqueneves81@gmail.com>
2024-07-18 19:34:10 -0300

    #7 Listagem de pacientes

--- src/app/pages/patient-list/patient-list.component.ts
+++ src/app/pages/patient-list/patient-list.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Patient } from '../../model/patient.model';
+import { PatientService } from '../../services/patient.service';
+import { Observable} from 'rxjs';
 
 @Component({
   selector: 'app-patient-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DatePipe],
   templateUrl: './patient-list.component.html',
   styleUrl: './patient-list.component.scss'
 })
 export class PatientListComponent {
 
+  private patientService = inject(PatientService);
+
+  patients$ = new Observable<Patient[]>();
+
+  ngOnInit(): void {
+    this.patients$ = this.patientService.getPatients();
+  }
+
+  editPatient(patient: Patient): void {
+    console.log('Editing patient:', patient);
+  }
+
+  deletePatient(patient: Patient): void {
+    console.log('Deleting patient:', patient);
+    this.patientService.deletePatient(patient.id).subscribe(() => {
+      this.patients$ = this.patientService.getPatients();
+    });
+  }
 }
